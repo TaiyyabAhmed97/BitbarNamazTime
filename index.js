02d28c2cf021b73aa55ee7dddc1a2b6a4bad36ea
Taiyyab Ahmed <tahmed@productiveedge.com>
2020-01-09 14:17:07 -0600

    regex to extract 25hr format time from date, function to create timings array

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-var axios = require("axios");
-var readlinesync = require("readline-sync");
-var city = readlinesync.question("Enter your City: ");
-var country = readlinesync.question("Enter your Country: ");
+#!/usr/bin/env /usr/local/bin/node
 
-console.log([city, country]);
+const axios = require("axios");
+//const readlinesync = require("readline-sync");
+const bitbar = require("bitbar");
+const NAMAZES = ["Fajr", "Sunrise", "Dhuhr", "Asr", "Maghrib", "Isha"];
+var city = "Chicago";
+var country = "USA";
+
 var url = ` http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1&school=1`;
 
-var timings = {};
+let pattern = /[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/;
+
+const timeNow = pattern.exec(new Date())[0];
+
 axios
   .get(url)
   .then(response => {
-    console.log(response.data.data);
     timings = response.data.data;
+    //console.log(timings.timings);
+
+    // Parse timings object into difference from current time
+    let newNamazArr = [];
+    let temp = Object.keys(timings.timings);
+    for (var i = 0; i < temp.length; i++) {
+      if (NAMAZES.includes(temp[i])) {
+        newNamazArr.push(timings.timings[temp[i]]);
+      }
+    }
+    console.log(newNamazArr);
+
+    // create array of time from now differnce
+
+    //getNamaz(timings, timeNow)
+    // feed into bitbar
+    bitbar([
+      {
+        text: `namaz`,
+        color: "blue",
+        dropdown: false
+      }
+    ]);
   })
   .catch(error => console.error(error));
+
+function getNamaz(namazes, timeNow) {
+  let curr;
+  for (const namaz in namazes) {
+  }
+}
