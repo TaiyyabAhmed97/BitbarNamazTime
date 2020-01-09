c14ca48cc6013f74c5eb77bea7b03708655a64c9
Taiyyab Ahmed <tahmed@productiveedge.com>
2020-01-09 14:39:31 -0600

    refactored + added comments

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ var country = "USA";
 
 var url = ` http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=1&school=1`;
 
-let pattern = /[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/;
+// REgExp to extract 24hr formatted js time
+let pattern = /[0-9][0-9]:[0-9][0-9]/;
 
 const timeNow = pattern.exec(new Date())[0];
+
+// https call to get today's namazes
 
 axios
   .get(url)
   .then(response => {
     timings = response.data.data;
-    //console.log(timings.timings);
 
-    // Parse timings object into difference from current time
+    // create array of times from namaz time object
     let newNamazArr = [];
     let temp = Object.keys(timings.timings);
     for (var i = 0; i < temp.length; i++) {
@@ -27,15 +29,17 @@ axios
         newNamazArr.push(timings.timings[temp[i]]);
       }
     }
-    console.log(newNamazArr);
 
-    // create array of time from now differnce
+    // get current namaz
 
-    //getNamaz(timings, timeNow)
+    let currentNamaz = getNamaz(newNamazArr, timeNow);
+
+    // get time diff from now till next namaz
+
     // feed into bitbar
     bitbar([
       {
-        text: `namaz`,
+        text: `${currentNamaz}, Time Left: ${"random"}`,
         color: "blue",
         dropdown: false
       }
@@ -44,7 +48,16 @@ axios
   .catch(error => console.error(error));
 
 function getNamaz(namazes, timeNow) {
+  //console.log(timeNow);
+  //onsole.log(namazes);
   let curr;
-  for (const namaz in namazes) {
+  for (var i = 0; i < namazes.length - 1; i++) {
+    if (timeNow > namazes[i] && timeNow < namazes[i + 1]) {
+      curr = NAMAZES[i];
+    }
+    if (timeNow == namazes[i]) {
+      curr = NAMAZES[i];
+    }
   }
+  return curr;
 }
